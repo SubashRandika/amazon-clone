1e5e80e17abf62388496ecc5b7b805e506bc95d6
Randika Rodrigo <randika.rodrigo@qmatic.com>
2020-09-16 13:51:37 +0530

    Completed Header UI component with searchBar and menu options including shopping cart icon.

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import Badge from "@material-ui/core/Badge";
+import { withStyles } from "@material-ui/core/styles";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingCartOutlinedIcon from "@material-ui/icons/ShoppingCartOutlined";
+import "./Header.styles.css";
+
+const StyledBadge = withStyles((theme) => ({
+	badge: {
+		backgroundColor: "#f08804",
+		fontFamily: "inherit",
+		fontSize: "14px",
+		fontWeight: "600",
+		color: "white"
+	}
+}))(Badge);
+
+function Header() {
+	return (
+		<div className='header'>
+			<div className='header__logoContainer'>
+				<img
+					className='header__logo'
+					src='http://pngimg.com/uploads/amazon/amazon_PNG11.png'
+					alt='Amazon Logo'
+				/>
+			</div>
+			<div className='header__search'>
+				<input type='text' className='header__searchInput' />
+				<SearchIcon className='header__searchIcon' />
+			</div>
+
+			<div className='header__nav'>
+				<div className='header__menuItem'>
+					<span className='header__menuItemTextOne'>Hello Guess</span>
+					<span className='header__menuItemTextTwo'>Sign In</span>
+				</div>
+				<div className='header__menuItem'>
+					<span className='header__menuItemTextOne'>Returns</span>
+					<span className='header__menuItemTextTwo'>& Orders</span>
+				</div>
+				<div className='header__menuItem'>
+					<span className='header__menuItemTextOne'>Your</span>
+					<span className='header__menuItemTextTwo'>Prime</span>
+				</div>
+				<div className='header__menuBasket'>
+					<StyledBadge badgeContent={0} invisible={false}>
+						<ShoppingCartOutlinedIcon className='header__basketIcon' />
+					</StyledBadge>
+					<div className='header__basketText'>Cart</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyAdfhJlamTd5D8qwJVSnaHskZd3H4c9y78",
+	authDomain: "clone-23a9e.firebaseapp.com",
+	databaseURL: "https://clone-23a9e.firebaseio.com",
+	projectId: "clone-23a9e",
+	storageBucket: "clone-23a9e.appspot.com",
+	messagingSenderId: "157850462043",
+	appId: "1:157850462043:web:a5bc12c143336ad7861c7e",
+	measurementId: "G-GTHTDVC8JH"
+};
