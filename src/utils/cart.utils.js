b578f23e0fabf15bb04a15d5e4564c97c64023cf
Randika Rodrigo <randika.rodrigo@qmatic.com>
2020-09-23 20:35:17 +0530

    Added optional chaining for collection iteration.

--- src/utils/cart.utils.js
+++ src/utils/cart.utils.js
@@ -2,26 +2,26 @@ export const addItemToCart = (cartItems, itemToAdd) => {
 	const existingItem = cartItems.find((item) => item.id === itemToAdd.id);
 
 	if (existingItem) {
-		return cartItems.map((item) => (item.id === itemToAdd.id ? { ...item, quantity: item.quantity + 1 } : item));
+		return cartItems?.map((item) => (item.id === itemToAdd.id ? { ...item, quantity: item.quantity + 1 } : item));
 	}
 
 	return [...cartItems, { ...itemToAdd, quantity: 1 }];
 };
 
 export const getTotalCartItems = (cartItems) => {
-	return cartItems.reduce((totalQuantity, currentItem) => totalQuantity + currentItem.quantity, 0);
+	return cartItems?.reduce((totalQuantity, currentItem) => totalQuantity + currentItem.quantity, 0);
 };
 
 export const getCartTotalPrice = (cartItems) => {
-	return cartItems.reduce((sumPrice, currentItem) => currentItem.price * currentItem.quantity + sumPrice, 0);
+	return cartItems?.reduce((sumPrice, currentItem) => currentItem.price * currentItem.quantity + sumPrice, 0);
 };
 
 export const removeItemFromCart = (cartItems, id) => {
-	return cartItems.filter((cartItem) => cartItem.id !== id);
+	return cartItems?.filter((cartItem) => cartItem.id !== id);
 };
 
 export const changeItemQuantity = (cartItems, item) => {
-	return cartItems.map((cartItem) =>
+	return cartItems?.map((cartItem) =>
 		cartItem.id === item.id ? { ...cartItem, quantity: Number(item.quantity) } : cartItem
 	);
 };
