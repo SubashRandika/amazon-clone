13859158a53427fdcf2adb18df1bc9bb5662def5
Randika Rodrigo <randika.rodrigo@qmatic.com>
2020-10-06 09:53:07 +0530

    Removed firebase details in firebase.config.js file.

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -3,11 +3,11 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyAdfhJlamTd5D8qwJVSnaHskZd3H4c9y78",
-	authDomain: "clone-23a9e.firebaseapp.com",
-	databaseURL: "https://clone-23a9e.firebaseio.com",
+	apiKey: "<YOUR_FIREBASE_API_KEY_HERE>",
+	authDomain: "<YOUR_FIREBASE_AUTH_DOMAIN_HERE>",
+	databaseURL: "<YOUR_FIREBASE_DB_URL_HERE>",
 	projectId: "clone-23a9e",
-	storageBucket: "clone-23a9e.appspot.com",
+	storageBucket: "<YOUR_FIREBASE_STORAGE_BUCKET_HERE>",
 	messagingSenderId: "157850462043",
 	appId: "1:157850462043:web:a5bc12c143336ad7861c7e",
 	measurementId: "G-GTHTDVC8JH"
